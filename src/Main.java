a28b42945cc8cf22a69ad714fa75f5865120ead0
Ali Omarov <omarovali425@gmail.com>
2023-02-27 23:19:37 +0300

    another commit

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,75 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
     static String path = "C://Games";
-    static String savegamsPath = "C://Games/savegames/";
+    static String savegamesPath = "C://Games/savegames/";
     static String zipPath = "C://Games/savegames/";
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-       createDir(path + "/src");
-        createDir(path + "/rec" );
-        createDir(path + "/savegames" );
-        createDir(path + "/temp");
-       createDir(path + "/src/main");
-        createDir(path + "/src/test");
+    public static void main(String[] args) {
+        List<String> files = Arrays.asList("/src", "/rec", "/savegames", "/temp", "/src/main", "/src/test", "/res/drawbles", "/res/icons");
+        for (String f : files) {
+            createDir(path + f);
+        }
         createFile(path + "/src/main/", "Main.java");
         createFile(path + "/src/main/", "Utils.java");
-        createDir(path + "/res/drawables");
-       createDir(path + "/res/vectors");
-        createDir(path + "/res/icons");
-        createFile(path + "/temp", "temp.java");
+        createFile(path + "/temp/", "temp.txt");
 
 
-    }
-
-    public static void createDir(String dirPath) {
-        StringBuilder sb = new StringBuilder();
-        File dir = new File(dirPath);
-        String text = "Папка создана";
-        sb.append(text);
-        if (dir.mkdir()) {
-            System.out.println(sb);
-            try {
-                FileWriter writer = new FileWriter(path + "/temp/temp.txt", true);
-                writer.write(String.valueOf(sb));
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
+        try (FileWriter writer = new FileWriter(path + "/temp/temp.txt", true)) {
+            writer.write((String.valueOf(sb)));
+            writer.append('\n');
+            writer.append('!');
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
-    }
-
-    public static void createFile(String dirPath, String fileName) {
-        StringBuilder sb = new StringBuilder();
-        File file = new File(dirPath, fileName);
-        String text = "Файл" + fileName + "создан";
-        sb.append(text);
-        try {
-            if (file.createNewFile()) {
-                System.out.println(text);
-                try {
-                    FileWriter writer = new FileWriter(path + "/temp/temp.txt", true);
-                    writer.write(String.valueOf(sb));
-                } catch (IOException ex) {
-                    System.out.println(ex.getMessage());
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
 
         GameProgress game1 = new GameProgress(10, 10, 10, 10);
         GameProgress game2 = new GameProgress(7, 7, 7, 7);
         GameProgress game3 = new GameProgress(5, 5, 5, 5);
-        saveGame(savegamsPath + "save1.dat", game1);
-        saveGame(savegamsPath + "save2.dat", game2);
-        saveGame(savegamsPath + "save3.dat", game3);
-        zipFiles(zipPath + "save.zip", savegamsPath);
 
-        delete(savegamsPath + "save1.dat");
-        delete(savegamsPath + "save2.dat");
-        delete(savegamsPath + "save3.dat");
+        saveGame(savegamesPath + "save1.dat", game1);
+        saveGame(savegamesPath + "save2.dat", game2);
+
+        saveGame(savegamesPath + "save3.dat", game3);
+
+        zipFiles(zipPath + "save.zip", savegamesPath);
+        delete(savegamesPath + "save1.dat");
+
+        delete(savegamesPath + "save2.dat");
+
+        delete(savegamesPath + "save3.dat");
     }
-    public static void saveGame(String savePath, GameProgress game) {
-        try (FileOutputStream fos = new FileOutputStream(savePath);
+
+    public static void createDir(String dirPath) {
+        File dir = new File(dirPath);
+        String text = "Папка создана";
+        // sb.append(text);
+        if (dir.mkdir()) {
+            sb.append(text + '\n');
+        }
+    }
+
+    public static void createFile(String dirPath, String fileName) {
+        File file = new File(dirPath, fileName);
+        String text = "Файл создан";
+        //sb.append(text);
+        try {
+            if (file.createNewFile())
+                sb.append(text + '\n');
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void saveGame(String savegamesPath, GameProgress game) {
+        try (FileOutputStream fos = new FileOutputStream(savegamesPath);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(game);
         } catch (Exception ex) {
@@ -81,28 +77,27 @@ public class Main {
         }
     }
 
-    public static void zipFiles(String zipPath, String savePath) {
-
+    public static void zipFiles(String zipPath, String savegamesPath) {
         try (FileOutputStream fos = new FileOutputStream(zipPath); ZipOutputStream zos = new ZipOutputStream(fos)) {
-
-            File dir = new File(savePath);
+            File dir = new File(savegamesPath);
             for (File item : dir.listFiles()) {
-                FileInputStream fis = new FileInputStream(savePath + item.getName());
+                FileInputStream fis = new FileInputStream(savegamesPath + item.getName());
                 ZipEntry entry = new ZipEntry(item.getName());
                 zos.putNextEntry(entry);
+                dir.delete();
                 byte[] buffer = new byte[fis.available()];
                 fis.read(buffer);
                 zos.write(buffer);
                 zos.closeEntry();
+                fis.close();
             }
 
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
-
-    public static void delete(String savePath) {
-        File file = new File(savePath);
+    public static void delete(String savegamesPath) {
+        File file = new File(savegamesPath);
         if (file.delete()) {
             System.out.println("Незаархивированные файлы удалены");
         } else {
